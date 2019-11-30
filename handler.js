1b5560a6271fe5099022d55524bbe491196dde38
Peter Jenkin <peternjenkin@gmail.com>
2019-11-30 14:38:26 +0000

    Another tiny text edit to push after re-saving Stage edits

--- handler.js
+++ handler.js
@@ -8,7 +8,7 @@ module.exports.logger = async event => {
     body: JSON.stringify(
       {
         message: 'Serverless CI/CD Demo',
-        version: "v4.0",
+        version: "v5.0",
         timestamp: moment().unix()
       })
   };
