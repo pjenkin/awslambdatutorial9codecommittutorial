febed06744107495e23da41f2518744f631866e5
Peter Jenkin <peternjenkin@gmail.com>
2019-11-30 12:08:48 +0000

    9-182 Text change for AWS CodePipeline CD

--- handler.js
+++ handler.js
@@ -8,7 +8,7 @@ module.exports.logger = async event => {
     body: JSON.stringify(
       {
         message: 'Serverless CI/CD Demo',
-        version: "v2.0",
+        version: "v3.0",
         timestamp: moment().unix()
       })
   };
